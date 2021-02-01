32b4061b0be8159dc7e15a15fa63e922c358d6aa
biladina <nabil_munawar@protonmail.ch>
2021-02-01 11:06:45 +0700

    perbaikan config

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=sikd',
-    'username' => 'r00t',
-    'password' => 'biladina',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
