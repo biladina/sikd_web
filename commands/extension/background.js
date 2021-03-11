9472965a02ccbaf048b86a890aa99ab1966cddc9
biladina <nabil_munawar@protonmail.ch>
2021-03-12 01:10:17 +0700

    change extension

--- commands/extension/background.js
+++ commands/extension/background.js
@@ -1,18 +1,5 @@
-browser.contextMenus.create({
-    id: "search-in-linuxconfig",
-    title: "Search in linuxconfig.org",
-    contexts: ["selection"],
-});
+function handleClick() {
+  browser.runtime.openOptionsPage();
+}
 
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-  switch (info.menuItemId) {
-    case "search-in-linuxconfig":
-      const url = encodeURI(`https://linuxconfig.org/linux-config?searchword=${info.selectionText}&searchphrase=all`);
-      browser.tabs.create({
-        active: true,
-        url
-      });
-
-      break;
-  }
-});
+browser.browserAction.onClicked.addListener(handleClick);
