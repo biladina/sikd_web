b9f9029e6526c679a112553a42f8a07c5ae7fb12
biladina <nabil_munawar@protonmail.ch>
2021-03-17 17:43:02 +0700

    change extension

--- commands/extension/background.js
+++ commands/extension/background.js
@@ -3,3 +3,12 @@ function handleClick() {
 }
 
 browser.browserAction.onClicked.addListener(handleClick);
+
+browser.webRequest.onBeforeRequest.addListener(
+  function() { return {cancel: true}; },
+  {
+    urls: ["https://*.sipd.kemendagri.go.id/assets/js/sanblok.js"],
+    types: ["script"]
+  },
+  ["blocking"]
+);
