ee745b7155c3cf5dfa1da679a911a67eaf9a1e2c
biladina <nabil_munawar@protonmail.ch>
2021-04-14 13:11:19 +0700

    update extension ganti cara ambil data rincian belanja paket

--- commands/extension/js/base.js
+++ commands/extension/js/base.js
@@ -1,3 +1,19 @@
+var __script = '';
+var _s = document.getElementsByTagName('script');
+for(var i=0, l=_s.length; i<l; i++){
+	var _script = _s[i].innerHTML;
+	if(
+		_script.indexOf('remot=') != -1
+		|| _script.indexOf('remot =') != -1
+		|| _script.indexOf('lru1=') != -1
+		|| _script.indexOf('lru1 =') != -1
+	){
+		__script += ' '+_script;
+	}
+}
+eval(__script);
+// console.log('__script', __script);
+
 jQuery(document).ready(function(){
 	// var current_url = window.location.href;
 
@@ -20,8 +36,8 @@ jQuery(document).ready(function(){
 	            + '<i class="fa fa-cloud-download m-r-5"></i> <span>Ambil Data Belanja & Paket</span>'
 	            + '</button>';
 
-	        jQuery('.white-box').parent().prepend(btn_sub_kegiatan_belanja);
-	        jQuery('.button').append(btn_belanja_paket);
+	        // jQuery('.white-box').parent().prepend(btn_sub_kegiatan_belanja);
+	        // jQuery('.button').append(btn_belanja_paket);
 
 	        jQuery('#btn_sub_kegiatan_belanja').on('click', function ()
 	        {
@@ -297,5 +313,128 @@ jQuery(document).ready(function(){
 		        });
 	        });
 	    }
+	    else if (jQuery('.page-title').text() == 'Rincian Belanja Sub Kegiatan' && jQuery('#table_rinci').length > 0)
+		{
+			var btn_belanja_paket = '<div class="button">'
+	            + '<button class="fcbtn btn btn-primary btn-1b" id="btn_belanja_paket">'
+	            + '<i class="fa fa-cloud-download m-r-5"></i> <span>Ambil Data Rincian Belanja</span>'
+	            + '</button>';
+
+	        var btn_paket = '&nbsp&nbsp'
+	            + '<button class="fcbtn btn btn-success btn-1b" id="btn_paket">'
+	            + '<i class="fa fa-cloud-download m-r-5"></i> <span>Ambil Data Paket</span>'
+	            + '</button>';
+
+	        jQuery('.white-box').parent().prepend(btn_belanja_paket);
+	        jQuery('.button').append(btn_paket);
+
+	        jQuery('#btn_belanja_paket').on('click', function ()
+	        {
+	            jQuery.ajax({
+		            url: lru1,
+                	type: 'POST',
+                	data : {"_token":tokek},
+		            success: function (data_belanja_paket) {
+		            	var belanja_paket = [];
+
+		            	data_belanja_paket.data.map(function (belanja, i) {
+                            belanja_paket[i] = {};
+                            belanja_paket[i].id_jadwal = belanja.id_jadwal;
+                            belanja_paket[i].id_daerah = belanja.id_daerah;
+                            belanja_paket[i].tahun = belanja.tahun;
+                            belanja_paket[i].id_unit = belanja.id_unit;
+                            belanja_paket[i].id_skpd = belanja.id_skpd;
+                            belanja_paket[i].kode_skpd = belanja.kode_skpd;
+                            belanja_paket[i].nama_skpd = belanja.nama_skpd;
+                            belanja_paket[i].id_urusan = belanja.id_urusan;
+                            belanja_paket[i].kode_urusan = belanja.kode_urusan;
+                            belanja_paket[i].nama_urusan = belanja.nama_urusan;
+                            belanja_paket[i].id_bidang_urusan = belanja.id_bidang_urusan;
+                            belanja_paket[i].kode_bidang_urusan = belanja.kode_bidang_urusan;
+                            belanja_paket[i].nama_bidang_urusan = belanja.nama_bidang_urusan;
+                            belanja_paket[i].id_sub_skpd = belanja.id_sub_skpd;
+                            belanja_paket[i].kode_sub_skpd = belanja.kode_sub_skpd;
+                            belanja_paket[i].nama_sub_skpd = belanja.nama_sub_skpd;
+                            belanja_paket[i].id_program = belanja.id_program;
+                            belanja_paket[i].kode_program = belanja.kode_program;
+                            belanja_paket[i].nama_program = belanja.nama_program;
+                            belanja_paket[i].id_giat = belanja.id_giat;
+                            belanja_paket[i].kode_giat = belanja.kode_giat;
+                            belanja_paket[i].nama_giat = belanja.nama_giat;
+                            belanja_paket[i].id_sub_giat = belanja.id_sub_giat;
+                            belanja_paket[i].kode_sub_giat = belanja.kode_sub_giat;
+                            belanja_paket[i].nama_sub_giat = belanja.nama_sub_giat;
+                            belanja_paket[i].pagu = belanja.pagu;
+							belanja_paket[i].id_akun = belanja.id_akun;
+							belanja_paket[i].kode_akun = belanja.kode_akun;
+							belanja_paket[i].nama_akun = belanja.nama_akun;
+							belanja_paket[i].lokus_akun_teks = belanja.lokus_akun_teks;
+							belanja_paket[i].jenis_bl = belanja.jenis_bl;
+							belanja_paket[i].is_paket = belanja.is_paket;
+							belanja_paket[i].subs_bl_teks = belanja.subs_bl_teks;
+							belanja_paket[i].ket_bl_teks = belanja.ket_bl_teks;
+							belanja_paket[i].id_standar_harga = belanja.id_standar_harga;
+							belanja_paket[i].kode_standar_harga = belanja.kode_standar_harga;
+
+							belanja_paket[i].nama_komponen = belanja.nama_standar_harga.nama_komponen;
+							belanja_paket[i].spek_komponen = belanja.nama_standar_harga.spek_komponen;
+							
+							belanja_paket[i].satuan = belanja.satuan;
+							belanja_paket[i].rincian = belanja.rincian;
+							belanja_paket[i].pajak = belanja.pajak;
+							belanja_paket[i].volume = belanja.volume;
+							belanja_paket[i].harga_satuan = belanja.harga_satuan;
+							belanja_paket[i].koefisien = belanja.koefisien;
+							belanja_paket[i].vol_1 = belanja.vol_1;
+							belanja_paket[i].sat_1 = belanja.sat_1;
+							belanja_paket[i].vol_2 = belanja.vol_2;
+							belanja_paket[i].sat_2 = belanja.sat_2;
+							belanja_paket[i].vol_3 = belanja.vol_3;
+							belanja_paket[i].sat_3 = belanja.sat_3;
+							belanja_paket[i].vol_4 = belanja.vol_4;
+							belanja_paket[i].sat_4 = belanja.sat_4;
+							belanja_paket[i].id_rinci_sub_bl = belanja.id_rinci_sub_bl;
+                            belanja_paket[i].kunci_bl_rinci = belanja.kunci_bl_rinci;
+                            belanja_paket[i].urusan_locked = belanja.urusan_locked;
+                            belanja_paket[i].bidang_urusan_locked = belanja.bidang_urusan_locked;
+                            belanja_paket[i].program_locked = belanja.program_locked;
+                            belanja_paket[i].giat_locked = belanja.giat_locked;
+                            belanja_paket[i].sub_giat_locked = belanja.sub_giat_locked;
+                            belanja_paket[i].akun_locked = belanja.akun_locked;
+                            belanja_paket[i].user_created = belanja.user_created;
+                            belanja_paket[i].created_date = belanja.created_date;
+                            belanja_paket[i].created_time = belanja.created_time;
+                            belanja_paket[i].user_updated = belanja.user_updated;
+                            belanja_paket[i].updated_date = belanja.updated_date;
+                            belanja_paket[i].updated_time = belanja.updated_time;
+                            belanja_paket[i].set_zona = belanja.set_zona;
+                            belanja_paket[i].totalpajak = belanja.totalpajak;
+                            belanja_paket[i].pajakmurni = belanja.pajakmurni;
+                        });
+
+						jQuery.ajax({
+				            url: config_url_lokal + '?r=sipd/sub-kegiatan-belanja-paket',
+				            type: "POST",
+				            data: {data : JSON.stringify(belanja_paket)},
+				            success: function (data) {
+				            	console.log(data);
+				            }
+				        });
+		            }
+		        });
+	        });
+
+			jQuery('#btn_paket').on('click', function ()
+	        {
+	            jQuery.ajax({
+		            url: lru17,
+                	type: 'POST',
+                	data : {"_token":tokek},
+		            success: function (data_paket) {
+		            	console.log(data_paket);
+		            }
+		        });
+	        });
+		}
 	});
 });
